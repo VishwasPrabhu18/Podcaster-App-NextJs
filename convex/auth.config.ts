cf107c17ac3f0f995277eef3f5dac5baeee97d50
Vishwas Prabhu <vishwasprabhu@qliclabs.com>
2024-07-10 16:00:24 +0530

    convex is added

---
+++ convex/auth.config.ts
@@ -0,0 +1,11 @@
+
+const authConfig = {
+  providers: [
+    {
+      domain: "https://giving-emu-8.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
+
+export default authConfig;
